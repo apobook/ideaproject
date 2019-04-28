5fd6cdbef2b1d86c458664649ea5e29ed316f848
thh_pro <apobook@163.com>
2019-04-29 01:26:29 +0800

    Merge branch 'master' of https://github.com/apobook/ideaproject
    # Conflicts:
    #	src/main/java/com/thh/spring/controller/SampleController.java

--- src/main/java/com/thh/spring/controller/SampleController.java
+++ src/main/java/com/thh/spring/controller/SampleController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 
 @RestController
-@Api(tags = "swaggerEEEEEEEEEEEEEEEEE1")
+@Api(tags = "swaggeruywuywuyewuy")
 public class SampleController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
