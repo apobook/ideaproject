618a5472d21b38507a076a940937cca822f5b7a7
thh_pro <apobook@163.com>
2019-04-29 00:55:45 +0800

    solve conflict

--- src/main/java/com/thh/spring/controller/SampleController.java
+++ src/main/java/com/thh/spring/controller/SampleController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 
 @RestController
-@Api(tags = "swaggerYYYY")
+@Api(tags = "swagger不接")
 public class SampleController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
