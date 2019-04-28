822d89395996f6dcf2ff49e5b51ad431dafc8003
thh_pro <apobook@163.com>
2019-04-29 01:06:09 +0800

    yyyy

--- src/main/java/com/thh/spring/controller/SampleController.java
+++ src/main/java/com/thh/spring/controller/SampleController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 
 @RestController
-@Api(tags = "swagger要解决")
+@Api(tags = "swaggerTTTTTTTTT")
 public class SampleController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
