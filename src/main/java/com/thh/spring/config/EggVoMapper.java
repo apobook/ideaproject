a247b9a19f8b0105135f03e4b32acb40578ec676
thh_pro <apobook@163.com>
2019-04-28 23:12:16 +0800

    test idea git

--- src/main/java/com/thh/spring/config/EggVoMapper.java
+++ src/main/java/com/thh/spring/config/EggVoMapper.java
@@ -3,7 +3,6 @@ package com.thh.spring.config;
 
 import com.thh.spring.bean.Egg;
 import com.thh.spring.bean.EggVo;
-import org.mapstruct.Mapper;
 
 //@Mapper(componentModel="spring")
 public interface EggVoMapper {
